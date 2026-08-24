499815acfeb81f0dec7f1f647b25d5764c96f1c1
Artur Koshtei <gh.public10110@gmail.com>
2026-08-24 21:32:58 +0300

    fix(skill): skip PDAM evasion without weapon

--- internal/gameserver/model/actor/creature/formula.go
+++ internal/gameserver/model/actor/creature/formula.go
@@ -78,7 +78,7 @@ func ResolvePhysicalSkillInput(caster DeathActor, target FormulaActor, def model
 		return formulas.PhysicalSkillInput{}, false
 	}
 	soulshot := attacker.SoulshotCharged()
-	if attacker.AttackType() != item.WeaponNone && attacker.AttackType() != item.WeaponBow && float64(target.Roll(100)) < target.CalcStat(stat.PSkillEvasion, 0) {
+	if attacker.AttackType() != item.WeaponFist && attacker.AttackType() != item.WeaponBow && float64(target.Roll(100)) < target.CalcStat(stat.PSkillEvasion, 0) {
 		return formulas.PhysicalSkillInput{Evaded: true}, true
 	}
 	skillPower := float64(def.Power)
